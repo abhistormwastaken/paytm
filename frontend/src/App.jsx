2fcbe0bf98683fcd39c22abafa0bfa8b22bcb51d
abhistormwastaken <abhistorm409@gmail.com>
2024-02-26 21:27:19 +0530

    frontend routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,16 @@
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
